98be5658b22c8d81bc717bd7f614cc3f5ac3ae82
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 03:41:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object[key] = value;
-return 
+return object;
+}
+
