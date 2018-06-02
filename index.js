fd0d08e057d9f50d19611cb87ca3daec3b65e011
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 03:40:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object = 
